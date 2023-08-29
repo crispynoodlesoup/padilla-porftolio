5c2871e8e0af80e0e978881853ab3ec15c47ca35
crispynoodlesoup <thepanman321@gmail.com>
2023-08-29 16:19:34 -0500

    random lines canvas background

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
 const glowCircles = [...document.querySelectorAll(".background-glow-animated")];
 
-for(let i = 0; i < glowCircles.length; i++) {
-    console.log(glowCircles);
-    glowCircles[i].style.animationDelay = `${i*0.6}s`;
+for (let i = 0; i < glowCircles.length; i++) {
+  console.log(glowCircles);
+  glowCircles[i].style.animationDelay = `${i * 0.6}s`;
 }
+
+const canvas = document.querySelector(".hero-canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const context = canvas.getContext("2d");
+
+function drawRandom() {
+  const totalIterations = window.innerWidth*1.2;
+  for (let i = 0; i < totalIterations; i++) {
+    let x = Number.parseInt(Math.random() * window.innerWidth);
+    let y = Number.parseInt(Math.random() * window.innerHeight);
+    let length = Math.random() * 20 + 10;
+    let angle = Math.random() * 2 * Math.PI;
+    let endX = x + Number.parseInt(Math.cos(angle) * length);
+    let endY = y + Number.parseInt(Math.sin(angle) * length);
+    let darkness = Number.parseInt(Math.random() * 11 + 5);
+  
+  
+    console.log(darkness);
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(endX, endY);
+    context.strokeStyle = `hsl(201, 100%, ${darkness}%)`;
+    context.stroke();
+  }
+}
+
+drawRandom();
+
+window.addEventListener('resize', function(event){
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  drawRandom();
+});
