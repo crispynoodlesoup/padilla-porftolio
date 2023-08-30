1d233e160711472f42e317c0420b90e8a768531a
crispynoodlesoup <thepanman321@gmail.com>
2023-08-30 13:46:36 -0500

    make each line a module for easy operations

--- app.js
+++ app.js
@@ -10,11 +10,57 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const context = canvas.getContext("2d");
+
+let mouse = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (e) => {
+  mouse.x = e.x;
+  mouse.y = e.y;
+});
+
+let line = (x, y, endX, endY, darkness) => {
+  origin = { x, y, endX, endY };
+
+  function draw() {
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(endX, endY);
+    context.strokeStyle = `hsl(201, 100%, ${darkness}%)`;
+    context.stroke();
+
+    // circles at ends
+    context.beginPath();
+    context.arc(x, y, 2, 0, 2 * Math.PI, false);
+    context.stroke();
+
+    context.beginPath();
+    context.arc(endX, endY, 2, 0, 2 * Math.PI, false);
+    context.stroke();
+  }
+
+  function update() {
+    
+  }
+
+  return {
+    origin,
+    x,
+    y,
+    endX,
+    endY,
+    draw,
+    update,
+  };
+};
+
 let lineArray;
 
 function generateLines() {
   lineArray = [];
-  
+
   const totalIterations = window.innerWidth;
   for (let i = 0; i < totalIterations; i++) {
     let x = Number.parseInt(Math.random() * window.innerWidth);
@@ -25,45 +71,22 @@ function generateLines() {
     let endY = y + Number.parseInt(Math.sin(angle) * length);
     let darkness = Number.parseInt(Math.random() * 11 + 5);
 
-    lineArray.push({
-      x,
-      y,
-      endX,
-      endY,
-      darkness,
-    });
+    lineArray.push(line(x, y, endX, endY, darkness));
   }
 }
 
 function drawLines() {
   lineArray.forEach((line) => {
-    context.beginPath();
-    context.moveTo(line.x, line.y);
-    context.lineTo(line.endX, line.endY);
-    context.strokeStyle = `hsl(201, 100%, ${line.darkness}%)`;
-    context.stroke();
-  
-    context.beginPath();
-    context.arc(line.x, line.y, 1, 0, 2 * Math.PI, false);
-    context.stroke();
-  
-    context.beginPath();
-    context.arc(line.endX, line.endY, 1, 0, 2 * Math.PI, false);
-    context.stroke();
-
-  }); 
+    line.update();
+    line.draw();
+  });
 }
 
 let circleX = 50;
 function animate() {
   context.clearRect(0, 0, window.innerWidth, window.innerHeight);
   drawLines();
-  context.beginPath();
-  context.strokeStyle = `green`;
-  context.arc(circleX, 50, 20, 0, 2 * Math.PI, false);
-  context.stroke();
 
-  circleX += 1;
   requestAnimationFrame(animate);
 }
 
@@ -71,7 +94,7 @@ generateLines();
 drawLines();
 animate();
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   context.clearRect(0, 0, window.innerWidth, window.innerHeight);
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
